d474781c7081d88c084e3d35de5b76bdc7661151
Mohamed Baaziz <m.baaziz@usec-utc.fr>
2015-05-01 01:17:59 +0200

    Tasks classes declaration

--- ProjectCalendar/Task.h
+++ ProjectCalendar/Task.h
@@ -20,9 +20,10 @@ class Task{
     enum TaskType {UNITARY, COMPOSITE} tasktype;
 
     Task(const QString& id, const QString& t, const Duration& dur, const QDate& dispo, const QDate& term):
-        identifier(id), title(t), duration(dur), disponibility(dispo), deadline(term), tasktype(setTaskType()){}
+        identifier(id), title(t), duration(dur), disponibility(dispo), deadline(term), tasktype(UNITARY){setTaskType();}
     Task(const Task& t);
-    Task& operator=(const Task&);
+    Task& operator=(const Task& t);
+    virtual ~Task(){}
     virtual void setTaskType() = 0;
 
     friend class TaskManger;
@@ -33,6 +34,10 @@ public:
     Duration getDuration() const {return duration;}
     QDate getDisponibility() const {return disponibility;}
     QDate getDeadline() const {return deadline;}
+    QString getTaskType() const {
+        if (tasktype == COMPOSITE) return "composite";
+        else return "unitary";
+    }
 
     void setId(const QString& id) {identifier=id;}
     void setTitle(const QString& t) {title=t;}
@@ -41,10 +46,116 @@ public:
     void setDeadline(const QDate& date) {deadline=date;}
 };
 
+class UnitaryTask: public Task {
+    bool preemptive;
+
+    UnitaryTask(const QString& id, const QString& t, const Duration& dur, const QDate& dispo, const QDate& term):
+        Task(id,t,dur,dispo,term), preemptive(true){setPreemptive();}
+    UnitaryTask(const UnitaryTask& t);
+    UnitaryTask& operator=(const UnitaryTask& t);
+    virtual ~UnitaryTask(){}
+    virtual void setPreemptive() = 0;
+    void setTaskType(){tasktype = UNITARY;}
+
+    friend class TaskManager;
+
+public:
+    bool isPreemptive() const {return preemptive;}
+
+};
+
+class PreemptiveTask: public UnitaryTask {
+    PreemptiveTask(const QString& id, const QString& t, const Duration& dur, const QDate& dispo, const QDate& term):
+        UnitaryTask(id,t,dur,dispo,term){}
+    PreemptiveTask(const PreemptiveTask& t);
+    PreemptiveTask& operator=(const PreemptiveTask& t);
+    ~PreemptiveTask(){}
+    void setPreemptive() {preemptive = true;}
+
+    friend class TaskManager;
+
+public:
+    void setInterruption();
+};
+
+class NonPreemptiveTask: public UnitaryTask {
+    NonPreemptiveTask(const QString& id, const QString& t, const Duration& dur, const QDate& dispo, const QDate& term):
+        UnitaryTask(id,t,dur,dispo,term){}
+    NonPreemptiveTask(const NonPreemptiveTask& t);
+    NonPreemptiveTask& operator=(const PreemptiveTask& t);
+    ~NonPreemptiveTask(){}
+    void setPreemptive() {preemptive = false;}
+
+    friend class TaskManager;
+};
+
+class CompositeTask: public Task {
+    Task** subTasks;
+
+    CompositeTask(const QString& id, const QString& t, const Duration& dur, const QDate& dispo, const QDate& term):
+        Task(id,t,dur,dispo,term){}
+    CompositeTask(const CompositeTask& t);
+    CompositeTask& operator=(const CompositeTask& t);
+    ~CompositeTask(){}
+    void setTaskType(){tasktype = COMPOSITE;}
+
+    friend class TaskManager;
+
+public:
+    /*!
+     * \brief addSubTask
+     * the subtask has to be created before, this method only adds the address
+       of this task in the array of subTasks.
+     */
+    void addSubTask(const Task& t);
+    /*!
+     * \brief removeSubTask
+     * this method destroys the subtask using TaskManager::removeTask method
+       and updates the composite-task's array of subtasks
+     */
+    void removeSubTask(const Task& t);
+    Task& getSubTask(const QString& id);
+};
+
+
+
 class TaskManager {
-    Task taches**;
-    int nb;
-    int max;
+    Task** tasks;
+    unsigned int nb;
+    unsigned int max;
+    QString file;
+
+    void addItem(Task* t);
+    Task* findTask(const QString& id) const;
+    TaskManager();
+    ~TaskManager();
+    TaskManager(const TaskManager& tm);
+    TaskManager& operator=(const TaskManager& tm);
+
+    struct Handler {
+        TaskManager* instance;
+        Handler():instance(0){}
+        ~Handler(){if (instance) delete instance;}
+    };
+    static Handler handler;
+
+public:
+    static TaskManager& getInstance();
+    static void libererInstance();
+    UnitaryTask& addUnitaryTask(const QString& id, const QString& t, const Duration& dur, const QDate& dispo, const QDate& term,bool preempt = false);
+    CompositeTask& addCompositeTask(const QString& id, const QString& t, const Duration& dur, const QDate& dispo, const QDate& term);
+    Task& getTask(const QString& id);
+    const Task& getTask(const QString& id) const;    // so that it could be used in const methods that shouldn't modify the task
+    bool isTaskHere(const QString& id) const {return findTask(id) != 0;}
+    void load(const QString& f);
+    void save(const QString& f);
+
+    /* TO DO :
+     * Add iterators declarations,
+     * Define in Task.cpp all the functions
+     * Think about new functions that could be usefull
+    */
+
 };
 
 #endif // TASK_H
