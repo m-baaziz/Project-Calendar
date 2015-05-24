0121672ec92ff7021c83360fcede9d56b8b640d7
Baaziz Mohamed-Lamine <mohamed-lamine.baaziz@etu.utc.fr>
2015-05-24 18:03:44 +0200

    isCompleted and progression (to preemptive tasks) attributs added, comments to prepare programation Management

--- ProjectCalendar/Task.h
+++ ProjectCalendar/Task.h
@@ -28,7 +28,7 @@ class Task{
     void checkCompositionValidity();
 protected:
     Task(const QString& id, const QString& t, const Duration& dur, const QDate& dispo, const QDate& term, const enum TaskType& type):
-        identifier(id), title(t), duration(dur), disponibility(dispo), deadline(term), taskType(type){}
+        identifier(id), title(t), duration(dur), disponibility(dispo), deadline(term), taskType(type), isCompleted(false){}
     virtual ~Task(){}
     QString identifier;
     QString title;
@@ -36,9 +36,12 @@ protected:
     QDate disponibility;
     QDate deadline;
     enum TaskType taskType;
+    bool isCompleted;
 
-    template<class T,class F>
-    friend class TaskFactory;
+    void setCompleted() {isCompleted = true;}   // when a programmation has been done, ProgrammationManager has to set the task as completed.
+                                                // a programmationManager can't really set a programmation for a CompositeTask, it has to set them for its subtasks
+    template<class T,class F>                   // a programmationManager when setting a programmation has to check Associations constraints.
+    friend class TaskFactory;                   // completed tasks have special treatments (special displaying for exemple ...)
 
 public:
     QString getId() const {return identifier;}
@@ -129,14 +132,20 @@ class PreemptiveTask: public UnitaryTask {
     PreemptiveTask& operator=(const PreemptiveTask& t);
 protected:
     PreemptiveTask(const QString& id, const QString& t, const Duration& dur, const QDate& dispo, const QDate& term):
-        UnitaryTask(id,t,dur,dispo,term,PREEMPTIVE){}
+        UnitaryTask(id,t,dur,dispo,term,PREEMPTIVE), progression(0){}
     virtual ~PreemptiveTask();
-
+    float progression;                 // when a programmation is set for a preemptive task, programmationManager has to know much this programmation is supposed to increase the task's progression
+                                       // when a programmation is completed, it will increase the preemptive task's progression, if progression reaches 100, ProgrammtionManager sets the task as completed.
     template<class T,class F>
     friend class UnitaryFactory;
 public:
     bool isPreemptive() const {return true;}
-    void setInterruption();
+    //void setInterruption();
+    void setProgression(const float& p) {
+        if (p < 0 || p > 100) throw CalendarException("Error : Invalid progression value (has to be >= 0 and <= 100)");
+        if (p < progression) throw CalendarException("Error : a Preemptive Task's progression can't decrease");
+        progression = p;
+    }
 };
 
 class NonPreemptiveTask: public UnitaryTask {
